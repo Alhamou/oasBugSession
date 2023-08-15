05ea875e59de9f42bd17aa3a12d12e8f0fba9c7b
Emad Alhamou <emadalhamo@gmail.com>
2023-08-15 14:47:18 +0300

    add remove cookies

--- background.js
+++ background.js
@@ -7,4 +7,15 @@
     const st = await chrome.storage.sync.get("test")
     console.log(st)
 
+    chrome.cookies.getAll({domain: "localhost:8080"}, function(cookies) {
+        for(var i=0; i<cookies.length;i++) {
+            console.log(cookies, cookies[i].domain, "__" ,cookies[i].path)
+            chrome.cookies.remove({url: "http://localhost:8080/formcycle", name: cookies[i].name});
+
+            // alert()
+        }
+    });
+
+
+
 })()
