b7a83c88b0d15b19a1aebbb63dcf6ad82a2a1b17
Emad Alhamou <emad.alhamou@ibykus.de>
2023-08-17 13:48:38 +0200

    adjusting

--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -3,23 +3,19 @@ const storageLoginData = localStorage.getItem("loginData") ? JSON.parse(localSto
 
 if(storageLoginData){
 
-    const {bnr, password, dnstBevolmecht, mandant, isActive = true} = storageLoginData
+    const {bnr, password, dnstBevolmecht, mandant} = storageLoginData
 
-    if(isActive){
-        try{
-            document.querySelector('[id="login-form:bnr"]').value = bnr
-            document.querySelector('[id="login-form:password"]').value = password
-            dnstBevolmecht ? document.querySelector("#switch_1_Text > label").click() : false
-            document.querySelector('[id="login-form:input-client-number"]').value = mandant
-    
-            setTimeout(() => {
-                document.querySelector('[id="login-form:login-btn"]').click()
-            }, 1000);
-        } catch { }
-        localStorage.removeItem("loginsData")
-    }
+    try{
+        document.querySelector('[id="login-form:bnr"]').value = bnr
+        document.querySelector('[id="login-form:password"]').value = password
+        dnstBevolmecht ? document.querySelector("#switch_1_Text > label").click() : false
+        document.querySelector('[id="login-form:input-client-number"]').value = mandant
 
-    
+        setTimeout(() => {
+            document.querySelector('[id="login-form:login-btn"]').click()
+        }, 1000);
+    } catch { }
+    localStorage.removeItem("loginsData")
 
 }
 
